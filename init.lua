d2e6eb2db4aab85f9f6fdaf6f6a734c3c01c4321
Seth Church <sethcdev@gmail.com>
2025-01-19 11:13:12 -0500

    initial utils

--- init.lua
+++ init.lua
@@ -1,18 +1,20 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+_G.EnderVim = require("plugins.utils")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
-require 'options'
+require("options")
 
 -- [[ Basic Keymaps ]]
-require 'keymaps'
+require("keymaps")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+require("lazy-bootstrap")
 
 -- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+require("lazy-plugins")
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/util/init.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.lsp = {}
+
+M.lsp.action = setmetatable({}, {
+  __index = function(_, action)
+    return function()
+      vim.lsp.buf.code_action({
+        apply = true,
+        context = {
+          only = { action },
+          diagnostics = {},
+        },
+      })
+    end
+  end,
+})
+
+return M
